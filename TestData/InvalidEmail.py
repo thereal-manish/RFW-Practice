f112d3482fa68cc09255e5c31075c7ba40cb57c4
Dhinesh <ndhinesh2001@gmail.com>
2023-05-16 11:47:56 +0530

    Fifth commit

--- TestData/InvalidEmail.py
+++
@@ -1,4 +0,0 @@
-Email_name="Lingam"
-Invalid_Email="dini125gmail"
-Email_mob="1234567880"
-Email_password="Lingam@123"
--- TestData/Invalidpass.py
+++
@@ -1,4 +0,0 @@
-pass_name="Kumaran"
-pass_Email="kumaran@gmail.com"
-pass_mob="1234567890"
-pass_password=""
